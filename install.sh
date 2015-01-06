8ef5df85174dd898eb510a559bfe813f4fbe4c90
Robert Krahn <robert.krahn@gmail.com>
2015-01-06 00:23:03 -0800

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
 git submodule update --init --recursive
+
+pushd LivelyKernel
+
+echo -e "INSTALLING NPM MODULES"
+npm install
+
+forever_installed=`npm list | grep "forever@" > /dev/null 2>&1`
+if [[ -z "$forever_installed" ]]; then
+  npm install forever
+fi
+
+echo -e "DOWNLOADING PARTSBIN"
+node -e "require('./bin/helper/download-partsbin.js')()";
+
+popd
