f005bffe50d1d518bbfc221501e247d1cc0faed3
internal-lively-web <unknown-user@internal.lively-web.org>
2015-01-09 07:04:12 +0000

    improved setup

--- install.sh
+++ install.sh
@@ -15,10 +15,16 @@ fi
 echo -e "DOWNLOADING PARTSBIN"
 export WORKSPACE_LK=`pwd`
 node -e "require('./bin/helper/download-partsbin.js')()";
+pushd PartsBin/Clojure;
+rm *;
+wget http://lively-web.org/PartsBin/Clojure/ClojureBrowser.{html,json,metainfo};
+wget http://lively-web.org/PartsBin/Clojure/ClojureController.{html,json,metainfo};
+popd
 popd
 
-cp cloxp/cloxp.html LivelyKernel/
+echo -e "MOVING LIVELY CUSTOMIZATIONS IN PLACE"
+
 cp cloxp/localconfig.js LivelyKernel/core/lively/
 cp cloxp/BetterConfig.js LivelyKernel/core/lively/ide/codeeditor/
 
-echo -e "INSTALLED"
+echo -e "INSTALLATION DONE"
