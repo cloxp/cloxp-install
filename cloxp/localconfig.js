5e1ac9fd8c4771bad21b5049cfdefe33f26f3cbd
lively-web <unknown-user@lively-web.org>
2015-01-13 09:34:49 +0000

    cloxp config setup for lively web deployment

--- cloxp/localconfig.js
+++ cloxp/localconfig.js
@@ -1,1 +1,10 @@
 lively.Config.add('modulesOnWorldLoad', "lively.ide.codeeditor.BetterConfig");
+
+// set uer based on cloxp assignment, also take care of the l2l connection
+// which won't work over the proxy
+var cookie = document.cookie.split(";").detect(function(ea) { return ea.include("cloxp-assignment"); });
+var port = cookie && Number(cookie.split("=").last());
+if (port) {
+  lively.whenLoaded(function(w) { $world.setCurrentUser("cloxp-user-" + port); })
+  lively.Config.addOption("nodeJSWebSocketURL", 'http://lively-web.org:' + port + '/nodejs');
+}
