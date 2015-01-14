afd0f2d73ca848998be7e65f4f200ab1c2f7c6db
lively-web <unknown-user@lively-web.org>
2015-01-14 08:14:36 +0000

    localconfig: ensure nodejswebsocket url

--- cloxp/localconfig.js
+++ cloxp/localconfig.js
@@ -2,8 +2,9 @@ lively.Config.add('modulesOnWorldLoad', "lively.ide.codeeditor.BetterConfig");
 
 // set uer based on cloxp assignment, also take care of the l2l connection
 // which won't work over the proxy
-var cookie = document.cookie.split(";").detect(function(ea) { return ea.include("cloxp-assignment"); });
-var port = cookie && Number(cookie.split("=").last());
+var cookie = document.cookie.split(";").filter(function(ea) { return ea.indexOf("cloxp-assignment") > -1; })[0];
+var cookieKV = cookie && cookie.split("=")
+var port = cookieKV && Number(cookieKV[1]);
 if (port) {
   lively.whenLoaded(function(w) { $world.setCurrentUser("cloxp-user-" + port); })
   lively.Config.addOption("nodeJSWebSocketURL", 'http://lively-web.org:' + port + '/nodejs');
