cce0eb92cca065567866ec3445c6c8f52a68573b
Robert Krahn <robert.krahn@gmail.com>
2015-08-10 02:51:04 +0200

    opening cloxp.html when running start.sh. For now on MacOs only. re: #4

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+if [[ `uname` == "Darwin" ]]; then
+    echo "__________________________________"
+    bash -c "sleep 3; open http://localhost:9001/cloxp.html" &
+fi
+
 pushd LivelyKernel
 node_modules/forever/bin/forever bin/lk-server.js \
   --db-config "{\"enableRewriting\":false}"
